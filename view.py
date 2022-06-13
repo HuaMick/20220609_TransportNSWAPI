7a2023d99ad7dbaf5b678055209954f46dc472cc
HuaMick <Hua.Mick@gmail.com>
2022-06-13 20:42:51 +1000

    20220613

--- view.py
+++ view.py
@@ -3,20 +3,26 @@ import controller
 import pandas as pd
 
 import streamlit as st
+from pandas_profiling import ProfileReport
 
 APPINFO = st.container()
-
-if 'API_JSON' not in st.session_state:
-    st.session_state['API_JSON'] = None
-
 APPINFO.subheader('Application Info')
 #APPINFO.write(st.session_state)
 
-st.title('NSW Transport API')
+st.title('20220612 - Transport NSW Simple API Request')
 OVERVIEW = st.container()
+
+OVERVIEW.write('author: Mick Hua')
+OVERVIEW.write('linkedin: https://www.linkedin.com/in/mick-hua-353353a/')
+OVERVIEW.write('github: https://github.com/HuaMick/20220609')
+
 OVERVIEW.subheader('Project Overview')
-OVERVIEW.write('This is a learning project where I dive into the Transport of NSW API') 
-OVERVIEW.write('I am using streamlit to build this web application as a blog') 
+OVERVIEW.write('This is a small learning project where I play with the Transport of NSW API') 
+OVERVIEW.write('Project Objectives:')
+OVERVIEW.write('- Pull the transport alerts data using the NSW API')
+OVERVIEW.write('- Interpret the API Data')
+OVERVIEW.write('- Transform the API Data into a usable format')
+OVERVIEW.write('If all goes well I should have a nice data frame.')
 
 STEP1 = st.container()
 STEP1.subheader('Getting Data From The API')
@@ -65,15 +71,27 @@ DF_ASDICT.write(df_container)
 
 STEP3.write('Now we can fill the dataframe by traversing the API Data') 
 STEP3.write('Theres a 1:many relationship between the alerts and the routs that are impacted')
+STEP3.write('We can do this by using nested for loops, code is a little complicated so if you would like the detail check out the GIT')
+STEP3.write('Also the code is pretty hacky, no real need to be too dynamic here so we can hardcode the structure')
+
 df_container = controller.DF_Populate(api_alerts,df_container)
 df = pd.DataFrame.from_dict(df_container)   
 STEP3.write(df)
 
+STEP5 = st.container()
+STEP5.subheader('Reflection')
+STEP5.write('From here theres a few interesting things Id like to do but its best left to the next iteration of the project.') 
+STEP5.write('So thats it for this one, being able to pull data from the API on command was the main objective here and so its time to move on') 
+STEP5.write('Though as a bonus I wanted to see if I could run the pandas data profiler, works pretty well see below .. :) ') 
 
+#profile = ProfileReport(df, minimal=True)
+#profile.to_file(output_file="output_min.html")
 
-
-
-
+import streamlit.components.v1 as components
+st.subheader('Pandas Data Profile of the Data')
+HtmlFile = open("output_min.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+components.html(source_code, width=1000, height=5000)
 
 #def UNPICKLE_JSON(Container):
 #    try:
